bc01e0a7b562c18864c5c8525243fcd7d80dcd9b
shadowrunner11 <pieroramirez810@gmail.com>
2023-03-31 16:28:22 -0500

    :lipstick: added styles and copy event listener

--- js/main.js
+++ js/main.js
@@ -5,26 +5,56 @@ const [
   buttonCipher,
   textAreaToCipher,
   cipheredTextContainer,
-  buttonDecipher
+  buttonDecipher,
+  cardContainer,
+  copyButton
 ] =  [
-  "cipher",
-  "to-cipher-text",
-  "ciphered-text",
-  "decipher"
-].map( id => document.getElementById(id))
+  '[data-button="cipher"]',
+  '[data-textarea="input-text"]',
+  '[data-result="text-result"]',
+  '[data-button="decipher"]',
+  '[data-card="result"]',
+  '[data-button="copy"]'
+].map(selector => document.body.querySelector(selector))
+
+
+function hideManIcon(){
+  cardContainer
+    .querySelector(".to-hide")
+    .classList.add("hidden")
+
+  cardContainer
+    .classList
+    .replace('justify-center', 'justify-between')
+
+  copyButton
+    .classList.remove("hidden")
+
+}
+
+copyButton?.addEventListener('click', async ()=>{
+  await navigator
+    .clipboard
+    .writeText(cipheredTextContainer.textContent)
+
+  console.log("Se copio con exito")
+
+})
 
 
 buttonCipher?.addEventListener('click', () => {
   const cipheredText = cipherByDict(textAreaToCipher.value)
 
   cipheredTextContainer.textContent = cipheredText
-
+  hideManIcon()
+  
 })
 
 buttonDecipher?.addEventListener('click', () => {
   const cipheredText = cipherByDict(textAreaToCipher.value, defaultReverseDict, false)
 
   cipheredTextContainer.textContent = cipheredText
+  hideManIcon()
 })
 
 
